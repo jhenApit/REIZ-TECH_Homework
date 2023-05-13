6f6ac01ce64f487a26477e9bd2197cfbb80671d5
Jhen Sarah Apit <jhensarah.apit@cit.edu>
2023-05-13 20:38:38 +0800

    Add Branch Class

---
+++ HomeworkAssignment/DepthCalculatorApp/Branch.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkAssignment.DepthCalculatorApp
+{
+    public class Branch
+    {
+        public List<Branch> branches;
+
+        public Branch()
+        {
+            branches = new List<Branch>();
+        }
+    }
+}
