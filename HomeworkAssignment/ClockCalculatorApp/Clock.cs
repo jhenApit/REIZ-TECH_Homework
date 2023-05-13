22ee2921f47be4ca91ab4d672b6cde03ec0e9d65
Jhen Sarah Apit <jhensarah.apit@cit.edu>
2023-05-13 20:13:16 +0800

    Add Clock Class

---
+++ HomeworkAssignment/ClockCalculatorApp/Clock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeworkAssignment.ClockCalculatorApp
+{
+    public class Clock
+    {
+        public int Hours { get; }
+        public int Minutes { get; }
+
+        public Clock(int hours, int minutes)
+        {
+            Hours = hours;
+            Minutes = minutes;
+        }
+    }
+}
