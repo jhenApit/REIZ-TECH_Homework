be1802f468d8d32d89cc6665ef9904e33eb11f94
Jhen Sarah Apit <jhensarah.apit@cit.edu>
2023-05-13 20:16:56 +0800

    Update Program File

---
+++ HomeworkAssignment/Program.cs
@@ -0,0 +1,22 @@
+﻿using HomeworkAssignment.ClockCalculatorApp;
+
+namespace HomeworkAssignment
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the time in the format of 'hh:mm' :");
+            string[] timeParts = Console.ReadLine()!.Split(':');
+
+            int hours = int.Parse(timeParts[0]);
+            int minutes = int.Parse(timeParts[1]);
+            Console.WriteLine("\n-----------------------------------------");
+
+            var clock = new Clock(hours, minutes);
+            var calculator = new ClockAngleCalculator(clock);
+
+            Console.WriteLine($"The angle between the hour and minute hands is: {calculator.CalculateLesserAngle()} degrees");
+        }
+    }
+}
